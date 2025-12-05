f6a56ab1bd7e027c12258f572cb2e9018d67f984
MacPRo <dwihplanner@gmail.com>
2025-12-05 16:45:17 +0700

    Refactor code structure for improved readability and maintainability

--- src/App.js
+++ src/App.js
@@ -5,17 +5,13 @@ import ContactSection from "./components/ContactSection";
 import FloatingContact from "./components/FloatingContact";
 import Footer from "./components/Footer";
 import Gallery from "./components/Gallery";
-// import GallerySCM from "./components/GallerySCM";
+import SCMGallery from "./components/SCMGallery";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import Product from "./components/Product";
 import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 import YouTubeSection from "./components/YouTubeSection";
 import Experience from "./components/Experience";
-
-
-import SkillChart from "./components/SkillChart";
 
 function App() {
   return (
@@ -24,14 +20,12 @@ function App() {
         <Navbar />
         <HeroSection />
         <About />
-        <Skills />
-        <SkillChart />
+        <SCMGallery />
         <div id="experience">
           <Experience />
         </div>
         <Projects />
         <Gallery />
-        {/* <GallerySCM /> */}
         <YouTubeSection />
         <Product />
         <ContactSection />
